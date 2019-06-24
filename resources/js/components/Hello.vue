969b8796847963de085523e1964a81f7017d2a76
STEVE-QUOCK\steve.quock <mr.quock@gmail.com>
2019-06-24 11:07:59 +0800

    test with laravel mix and vue

---
+++ resources/js/components/Hello.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Hello</h1>
+        <p class="hello">Hi {{ name || '柒!!' }}</p>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['name'],
+        data () {
+            return {
+                msg: 'This is a Laravel with Vue Demo.'
+            }
+        }
+    }
+</script>
+
+<style>
+.hello {
+    font-size: 2em;
+    color: green;
+}
+</style>
