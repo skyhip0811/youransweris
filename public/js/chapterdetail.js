1c26937fadaa140d00899f975e90744f8cb8493a
STEVE-QUOCK\steve.quock <mr.quock@gmail.com>
2019-06-25 18:27:07 +0800

    developing charter detail

--- public/js/chapterdetail.js
+++ public/js/chapterdetail.js
@@ -81,7 +81,7 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -110004,6 +110004,43 @@ if (token) {
 
 /***/ }),
 
+/***/ "./resources/js/chapterdetail.js":
+/*!***************************************!*\
+  !*** ./resources/js/chapterdetail.js ***!
+  \***************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _components_Remotesearch__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/Remotesearch */ "./resources/js/components/Remotesearch.vue");
+/* harmony import */ var _components_Headnav__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/Headnav */ "./resources/js/components/Headnav.vue");
+/* harmony import */ var _components_Chapterrow__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/Chapterrow */ "./resources/js/components/Chapterrow.vue");
+/* harmony import */ var _components_Bookrow__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./components/Bookrow */ "./resources/js/components/Bookrow.vue");
+__webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
+
+
+
+
+ // Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+new Vue({
+  el: '#app',
+  components: {
+    Headnav: _components_Headnav__WEBPACK_IMPORTED_MODULE_1__["default"],
+    Chapterrow: _components_Chapterrow__WEBPACK_IMPORTED_MODULE_2__["default"],
+    Bookrow: _components_Bookrow__WEBPACK_IMPORTED_MODULE_3__["default"],
+    Remotesearch: _components_Remotesearch__WEBPACK_IMPORTED_MODULE_0__["default"]
+  },
+  data: function data() {
+    return {
+      visible: false
+    };
+  }
+});
+
+/***/ }),
+
 /***/ "./resources/js/components/Bookrow.vue":
 /*!*********************************************!*\
   !*** ./resources/js/components/Bookrow.vue ***!
@@ -110334,75 +110371,14 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./resources/js/homepage.js":
-/*!**********************************!*\
-  !*** ./resources/js/homepage.js ***!
-  \**********************************/
-/*! no exports provided */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _components_Remotesearch__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/Remotesearch */ "./resources/js/components/Remotesearch.vue");
-/* harmony import */ var _components_Headnav__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/Headnav */ "./resources/js/components/Headnav.vue");
-/* harmony import */ var _components_Chapterrow__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/Chapterrow */ "./resources/js/components/Chapterrow.vue");
-/* harmony import */ var _components_Bookrow__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./components/Bookrow */ "./resources/js/components/Bookrow.vue");
-__webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
-
-
-
-
- // Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-new Vue({
-  el: '#app',
-  components: {
-    Headnav: _components_Headnav__WEBPACK_IMPORTED_MODULE_1__["default"],
-    Chapterrow: _components_Chapterrow__WEBPACK_IMPORTED_MODULE_2__["default"],
-    Bookrow: _components_Bookrow__WEBPACK_IMPORTED_MODULE_3__["default"],
-    Remotesearch: _components_Remotesearch__WEBPACK_IMPORTED_MODULE_0__["default"]
-  },
-  data: function data() {
-    return {
-      visible: false
-    };
-  }
-});
-
-/***/ }),
-
-/***/ "./resources/sass/app.scss":
-/*!*********************************!*\
-  !*** ./resources/sass/app.scss ***!
-  \*********************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-
-/***/ "./resources/sass/chapterdetail.scss":
-/*!*******************************************!*\
-  !*** ./resources/sass/chapterdetail.scss ***!
-  \*******************************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-
-/***/ 0:
-/*!******************************************************************************************************!*\
-  !*** multi ./resources/js/homepage.js ./resources/sass/app.scss ./resources/sass/chapterdetail.scss ***!
-  \******************************************************************************************************/
+/***/ 1:
+/*!*********************************************!*\
+  !*** multi ./resources/js/chapterdetail.js ***!
+  \*********************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\projects\youransweris\youransweris\resources\js\homepage.js */"./resources/js/homepage.js");
-__webpack_require__(/*! D:\projects\youransweris\youransweris\resources\sass\app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! D:\projects\youransweris\youransweris\resources\sass\chapterdetail.scss */"./resources/sass/chapterdetail.scss");
+module.exports = __webpack_require__(/*! D:\projects\youransweris\youransweris\resources\js\chapterdetail.js */"./resources/js/chapterdetail.js");
 
 
 /***/ })
---
+++ resources/views/charterdetail.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="csrf-token">
+  <!-- import CSS -->
+  <link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">
+  <link rel="stylesheet" type="text/css" href="/css/app.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <div id="app">
+    <el-container>
+    <el-header  class='head-menu'><headnav></headnav></el-header>
+    <el-main>
+    
+    </el-main>
+    </el-container>
+  </div>
+</body>
+  <script src="/js/chapterdetail.js"></script>
+  <script>
+
+  </script>
+</html>
