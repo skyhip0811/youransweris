8f7ec538a8580dae93100ed91103a76b69baafdf
STEVE-QUOCK\steve.quock <mr.quock@gmail.com>
2019-06-26 18:14:00 +0800

    change something in homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -25,7 +25,7 @@
               <el-col :span="4"><h5> 更新時間 </h5></el-col>
               <el-col :span="8"><h5> 書名 </h5></el-col>
               <el-col :span="8"><h5> 章節 </h5></el-col>
-              <el-col :span="4"><h5> 作者 </h5></el-col>
+              <el-col :span="4"><h5> 第一作者 </h5></el-col>
             </el-row>
            <chapterrow book='很久很久以前' chapter='山上有座廟' aurthor='作者二號'></chapterrow>
            <chapterrow book='中二病闖世界' chapter='我正式宣佈我有病' aurthor='他她牠'></chapterrow>
@@ -42,7 +42,7 @@
           <div  class="text item">
            <el-row>
               <el-col :span="14"><h5> 書名 </h5></el-col>
-              <el-col :span="6"><h5> 作者 </h5></el-col>
+              <el-col :span="6"><h5> 第一作者 </h5></el-col>
               <el-col :span="4"><h5> 觀看數 </h5></el-col>
             </el-row>
             <bookrow book="很久很久以前" aurthor='作者二號' ticket='139'></bookrow>
