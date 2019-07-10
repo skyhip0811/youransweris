3275c9bb8beb61dbcaed44ef93cb403926326719
STEVE-QUOCK\steve.quock <mr.quock@gmail.com>
2019-07-10 17:19:50 +0800

    fixed bugs

--- resources/views/charterdetail.blade.php
+++ resources/views/charterdetail.blade.php
@@ -6,6 +6,7 @@
   <!-- import CSS -->
   <link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">
   <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <link rel="stylesheet" type="text/css" href="/css/chapterdetail.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
    <script src="/js/manifest.js"></script>
  <script src="/js/vendor.js"></script>
