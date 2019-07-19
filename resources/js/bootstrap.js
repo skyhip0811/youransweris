b11fb7de8b9cca5fae561ca2b80c7a6fcecdc0d4
STEVE-QUOCK\steve.quock <mr.quock@gmail.com>
2019-07-19 12:09:21 +0800

    added google analytic

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -56,3 +56,12 @@ if (token) {
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     encrypted: true
 // });
+
+//Gtagjs
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-144193337-1', 'auto');
+ga('send', 'pageview');
