06fc736853fd3a05816ffb7aa2d0c0a079a0568e
STEVE-QUOCK\steve.quock <mr.quock@gmail.com>
2019-11-29 16:27:21 +0800

    fb like og

--- resources/views/chapterdetail.blade.php
+++ resources/views/chapterdetail.blade.php
@@ -3,17 +3,18 @@
 <head>
   <meta charset="UTF-8">
   <meta name="csrf-token">
-
-
-  <title> 蝴說 | {{$book->name}} | {{$chapter->name}}</title>
-
-   <!-- You can use open graph tags to customize link previews.
-    Learn more: https://developers.facebook.com/docs/sharing/webmasters -->
   <meta property="og:url"           content="https://butterfliessay.com/chapter/{{$chapter->id}}" />
   <meta property="og:type"          content="website" />
   <meta property="og:title"         content="蝴說 | {{$book->name}} | {{$chapter->name}}" />
   <meta property="og:description"   content="{{substr($chapter->content, 0, 100)}}" />
   <meta property="og:image"         content="{{asset('storage/'.$book->cover)}}" />
+  <meta property="fb:app_id"        content="1033646243639089"/>
+
+  <title> 蝴說 | {{$book->name}} | {{$chapter->name}}</title>
+
+   <!-- You can use open graph tags to customize link previews.
+    Learn more: https://developers.facebook.com/docs/sharing/webmasters -->
+  
   <!-- import CSS -->
   <link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">
   <link rel="stylesheet" type="text/css" href="/css/app.css?v={{ env('js_version_number') }}">
