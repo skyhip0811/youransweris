ee1efd6204f2c061ae2a26e919491d3b47384bae
STEVE-QUOCK\steve.quock <mr.quock@gmail.com>
2019-12-02 11:48:01 +0800

    fix additional infor

--- database/migrations/2019_12_02_114449_fix-additionalinfo.php
+++ database/migrations/2019_12_02_114449_fix-additionalinfo.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserReadsTable extends Migration
+class FixAdditionalinfo extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,11 @@ class CreateUserReadsTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_reads', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
+        Schema::table('chapters', function (Blueprint $table) {
+  
+            $table->text('additionalinfo')->change();
+
+
         });
     }
 
@@ -26,6 +28,6 @@ class CreateUserReadsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_reads');
+        //
     }
 }
